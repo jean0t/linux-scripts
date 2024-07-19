138b125d970f100ce7fb093e8d19713fc0dd4945
Jeanot <127698173+jean0t@users.noreply.github.com>
2024-07-19 16:09:19 -0300

    Update manage_download.sh

--- organization/manage_download.sh
+++ organization/manage_download.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This script scans your downloads directory to see if there is any new file
 # if there is, it will move to the correspondent directory, be it audios, videos or anything else
@@ -11,51 +11,62 @@ texts_dir="$download_dir/texts"
 binaries_dir="$download_dir/binaries"
 audio_dir="$download_dir/audio"
 videos_dir="$download_dir/videos"
+special_dir="$download_dir/special"
 
 
 # creates the directories if they don't exist
-if [ ! -d $images_dir ]; then
-	mkdir "$images_dir" "$texts_dir" "$binaries_dir" "$audio_dir"
+if [[ -e "$texts_dir" ]] && [[ -e "$images_dir" ]] && [[ -e "$special_dir" ]]; then
+	mkdir -p "$images_dir" "$texts_dir" "$binaries_dir" "$audio_dir" "$special_dir"
 fi
-
-#mkdir -p images_dir texts_dir binaries_dir audio_dir
 
 
 organize_files() {
 
-	for file in $(find "$1" -maxdepth 1 -type f); do
+	for file in $(cd "$1"; ls); do
+
+		[[ -d "$file" ]] && continue # if it is a directory, ignores
+
 			case "$(file --mime-type -b "$file")" in
 				image/*)
-					mv "$file" "$images_dir"
-					echo "$file moved"
-					;;
+					mv "$file" -t "$images_dir"
+					echo "$file moved to $images_dir"
+				;;
+
 				text/*)
-					mv "$file" "$texts_dir"
-					echo "$file moved"
-					;;
+					mv "$file" -t "$texts_dir"
+					echo "$file moved to $texts_dir"
+				;;
+
 				audio/*)
-					mv "$file" "$audio_dir"
-					echo "$file moved"
-					;;
+					mv "$file" -t "$audio_dir"
+					echo "$file moved to $audio_dir"
+				;;
+
 				application/*)
-					mv "$file" "$binaries_dir"
-					echo "$file moved"
-					;;
+					mv "$file" -t "$binaries_dir"
+					echo "$file moved to $binaries_dir"
+				;;
+
 				video/*)
-					mv "$file" "$videos_dir"
-					echo "$file moved"
-					;;
+					mv "$file" -t "$videos_dir"
+					echo "$file moved to $videos_dir"
+				;;
+
+				*)
+					mv "$file" -t "$special_dir"
+					echo "$file moved to $special_dir"
+				;;
 			esac
 		done
 	}
 
 
-count_of_scans=1
+count_of_scans='1'
 while true; do
-	rename "s/ /_/g" "$download_dir/*.*" # rename every file to avoid conflits with white space
+	 rename "s/ /_/g" "$download_dir/*.*" # rename every file to avoid conflits with white space
 	organize_files "$download_dir"
 	echo ""
-	echo "Downloads scan number ${count_of_scans} "
-	((count_of_scans++))	
+	echo "Downloads scan number ${count_of_scans}"
+	let count_of_scans++	
 	sleep 600 # scans the download directory each 10 minutes
 done
