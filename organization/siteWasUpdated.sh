b8809d40ddc3a2ee1f3a7b84c66efbf19a1f1f4d
Jeanot <127698173+jean0t@users.noreply.github.com>
2024-07-22 22:06:35 -0300

    Update siteWasUpdated.sh

--- organization/siteWasUpdated.sh
+++ organization/siteWasUpdated.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-read -p "Website: " URL
+[[ $( ping -c1 8.8.8.8 >&- 2>&1 ) ]] || { echo 'You do not have a network connection.' ; exit 1 ; }
+[[ -n "$1" ]] || { echo 'You must pass a URL' ; exit 1 ; }
+
+case "$1" in
+	-h|--help) { echo -e "Send a URL after call the program to be analyzed\nExample:\tsiteWasUpdated google.com" ; exit 0 ; } ;;
+ 	*) { echo "This command isn't recognized" ; exit 1 ; } ;;
+esac
+
+read -p "Website: " $1 # takes the website from the command
 OLD_CHECKSUM=$(curl -s "$URL" | md5sum | cut -d ' ' -f1)
 
 check_site() {
@@ -13,8 +21,8 @@ check_site() {
 	fi
 }
 
-
+# loop to check the website regularly
 while true; do
 	check_site $URL $OLD_CHECKSUM
-	sleep 100
+	sleep 300 # waits for 5 minutes to check if the website was updated 
 done
