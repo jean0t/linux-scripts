b0787351c5b09b4ad5806e81d9eb439d9224d19e
jean0t <joaomouryf@hotmail.com>
2025-01-29 16:41:47 -0300

    mount_device initiated

---
+++ organization/mount_device.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+
+########################################################## HEADER
+#
+# Author: Joao Mauricio
+# Github: Jean0t
+#
+# License: GPL-3
+#
+########################################################## INFO
+#
+# Compatibility: Bash and Zsh
+#
+# This script Goal is to allow automate mounting points
+# to flash drives
+#
+########################################################## CHANGELOG
+#
+#
+#
+#
+########################################################## CONFIG
+
+temp_dir="/mnt/$(date '+%F-%R')"
+
+
