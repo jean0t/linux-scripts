cbdb5e2892bb8f1f1d95556bfc989c4629656b7e
jena0t <joaomouryf@hotmail.com>
2025-02-13 22:50:13 -0300

    feat: mount_device 1.0 released

--- utilities/mount_device.sh
+++ utilities/mount_device.sh
@@ -17,11 +17,62 @@
 #
 ########################################################## CHANGELOG
 #
+# Version 1.0:
+#   - Support to mount one device automatically
+#   - Allow to choose if the user will want to identify
+#     the device from label or uuid.
 #
 #
-#
+########################################################## HELPER
+
+if [ "${1}" = '-v' ] || [ "${1}" = "--version" ] ; then
+  echo 'version 1.0'
+  exit 0 
+fi
+
+if [ "${1}" = '-h' ] || [ "${1}" = '--help' ] ; then
+  echo "
+  Usage: ./mount_device.sh [OPTIONS]
+
+  OPTIONS:
+    -v | --version  get the version of the script
+    -h | --help     get help
+
+  CONFIGURATION:
+    Run the command \`lsblk -o name,uuid,label,size\`
+    after that you select or the label or the uuid,
+    both are used to recognize the device
+
+    Open the script and then set the UUID or LABEL
+    variable with the device you want.
+
+    The device will be mounted in the home of the user.
+"
+  exit 0
+fi
+
 ########################################################## CONFIG
 
-temp_dir="/mnt/$(date '+%F-%R')"
+# Put the content inside the 'here'
+UUID=''
+LABEL=''
 
+########################################################## START
 
+# Must be root to work with filesystem
+[ $UID -ne 0 ] && exit 1
+
+# Two blocks depending on the option used if by UUID or LABEL
+if [ -n $UUID ]; then
+  DIRECTORY="$HOME/$UUID"
+  findfs UUID="${UUID}" 1>/dev/null 2>&1 || exit 1
+  mkdir -p "$DIRECTORY"
+  mount $(findfs UUID=$UUID) "$DIRECTORY"
+fi
+
+if [ -n $LABEL ]; then
+  DIRECTORY="$HOME/$LABEL"
+  findfs LABEL=${LABEL} 1>/dev/null 2>&1 || exit 1
+  mkdir -p "$DIRECTORY"
+  mount $(findfs LABEL=$LABEL) "$DIRECTORY"
+fi
