dc2d4be047753937cf86783d7043274709c73945
jena0t <joaomouryf@hotmail.com>
2025-02-15 16:45:03 -0300

    refactor: code now uses case statement

--- utilities/mount_device.sh
+++ utilities/mount_device.sh
@@ -15,6 +15,9 @@
 # This script Goal is to allow automate mounting points
 # to flash drives
 #
+# The script will not send output because it is made to be 
+# used with the crontab to detect the device
+#
 ########################################################## CHANGELOG
 #
 # Version 1.0:
@@ -22,24 +25,64 @@
 #   - Allow to choose if the user will want to identify
 #     the device from label or uuid.
 #
+# Version 1.1:
+#   - Code refactored: Now uses case instead of if's 
 #
-########################################################## HELPER
+########################################################## 
 
-if [ "${1}" = '-v' ] || [ "${1}" = "--version" ] ; then
-  echo 'version 1.0'
-  exit 0 
-fi
 
-if [ "${1}" = '-h' ] || [ "${1}" = '--help' ] ; then
-  echo "
+########################################################## CONFIG
+
+# Put the content inside one of them or both it will be used
+# to Identify the device
+UUID=''
+LABEL=''
+
+# Do not modify the version number
+VERSION='1.1'
+
+########################################################## START
+
+# Must be root to work with filesystem
+[ $UID -ne 0 ] && exit 1
+
+case "$1" in
+  
+  -v|--version)
+    echo "Version $VERSION"
+  ;;
+
+  # Two blocks depending on the option used if by UUID or LABEL
+ -u|--uuid)
+    DIRECTORY="$HOME/$UUID"
+    findfs UUID="${UUID}" 1>/dev/null 2>&1 || exit 1
+    mkdir -p "$DIRECTORY"
+    mount $(findfs UUID=$UUID) "$DIRECTORY"
+  ;;
+
+  
+  -l|--label)
+    DIRECTORY="$HOME/$LABEL"
+    findfs LABEL=${LABEL} 1>/dev/null 2>&1 || exit 1
+    mkdir -p "$DIRECTORY"
+    mount $(findfs LABEL=$LABEL) "$DIRECTORY"
+  ;;
+
+  
+  *)  
+  echo '
   Usage: ./mount_device.sh [OPTIONS]
 
   OPTIONS:
     -v | --version  get the version of the script
     -h | --help     get help
+    -u | --uuid     use the UUID to identify the device
+    -l | --label    use the LABEL to identify the device
 
   CONFIGURATION:
-    Run the command \`lsblk -o name,uuid,label,size\`
+    Run the command:
+    lsblk -o name,uuid,label,size
+
     after that you select or the label or the uuid,
     both are used to recognize the device
 
@@ -47,32 +90,7 @@ if [ "${1}" = '-h' ] || [ "${1}" = '--help' ] ; then
     variable with the device you want.
 
     The device will be mounted in the home of the user.
-"
-  exit 0
-fi
+  '
+  ;;
 
-########################################################## CONFIG
-
-# Put the content inside the 'here'
-UUID=''
-LABEL=''
-
-########################################################## START
-
-# Must be root to work with filesystem
-[ $UID -ne 0 ] && exit 1
-
-# Two blocks depending on the option used if by UUID or LABEL
-if [ -n $UUID ]; then
-  DIRECTORY="$HOME/$UUID"
-  findfs UUID="${UUID}" 1>/dev/null 2>&1 || exit 1
-  mkdir -p "$DIRECTORY"
-  mount $(findfs UUID=$UUID) "$DIRECTORY"
-fi
-
-if [ -n $LABEL ]; then
-  DIRECTORY="$HOME/$LABEL"
-  findfs LABEL=${LABEL} 1>/dev/null 2>&1 || exit 1
-  mkdir -p "$DIRECTORY"
-  mount $(findfs LABEL=$LABEL) "$DIRECTORY"
-fi
+esac
