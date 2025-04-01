7c8ade818f1606140d4626e7c539d708a800af5e
Jeanot <127698173+jean0t@users.noreply.github.com>
2025-04-01 14:18:32 +0000

    Update bash_auto_completion.sh

--- set_up_environment/bash_auto_completion.sh
+++ set_up_environment/bash_auto_completion.sh
@@ -4,7 +4,12 @@
 [ "$UID" -ne 0 ] && { printf "%s\n" "You must be root to run this script" ; exit 1 ; }
 
 
+# path to the file in the home directory of the user
 USER_FILE="$HOME/.inputrc"
+
+# retrieve contents
 cat /etc/inputrc > "$USER_FILE"
+
+# make the magic happens, allows the insensitive case completion
 echo 'set completion-ignore-case on' >> "$USER_FILE"
 echo 'set show-all-if-ambiguous on' >> "$USER_FILE"
