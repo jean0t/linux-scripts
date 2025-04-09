1571d06e93663b0119b674cc5732fb3c0804a128
Jeanot <127698173+jean0t@users.noreply.github.com>
2025-04-09 14:01:41 -0300

    Update organize_downloads.py

--- organization/organize_download_dir/organize_downloads.py
+++ organization/organize_download_dir/organize_downloads.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
 from pathlib import Path
+from datetime import datetime
+
+TIMENOW = datetime.now().strftime("%d/%m %H:%M")
+
+def message(filename, target_dir):
+	print(f"{TIMENOW} {filename} was moved to {target_dir}")
 
 def make_dir(path_obj):
 	path_obj.mkdir(parents=True, exist_ok=True)
@@ -11,43 +17,45 @@ downloads = home.joinpath("Downloads")
 for archive in downloads.iterdir():
 	if archive.is_file():
 		extension = archive.suffix
+        if extension == ".part":
+            continue
 
 		match extension:
 			case ".pdf"|".txt"|".md":
 				target_dir = downloads / "texts"
 				make_dir(target_dir)
 				archive.rename(target_dir / archive.name)
-				print(f"{archive.name} was moved to {target_dir}")
+				message(archive.name, target_dir)
 
 			case ".deb"|".appimage"|"":
 				target_dir = downloads / "binaries"
 				make_dir(target_dir)
 				archive.rename(target_dir / archive.name)
-				print(f"{archive.name} was moved to {target_dir}")
+				message(archive.name, target_dir)
 
-			case ".mp4"|".wav":
+			case ".mp4"|".wav"|".mkv":
 				target_dir = downloads / "videos"
 				make_dir(target_dir)
 				archive.rename(target_dir / archive.name)
-				print(f"{archive.name} was moved to {target_dir}")
+				message(archive.name, target_dir)
 
 			case ".mp3":
 				target_dir = downloads / "audio"
 				make_dir(target_dir)
 				archive.rename(target_dir / archive.name)
-				print(f"{archive.name} was moved to {target_dir}")
+				message(archive.name, target_dir)
 
 			case ".jpeg"|".jpg"|".png"|".gif":
 				target_dir = downloads / "images"
 				make_dir(target_dir)
 				archive.rename(target_dir / archive.name)
-				print(f"{archive.name} was moved to {target_dir}")
+				message(archive.name, target_dir)
 
 			case _:
 				target_dir = downloads / "special"
 				make_dir(target_dir)
 				archive.rename(target_dir / archive.name)
-				print(f"{archive.name} was moved to {target_dir}")
+				message(archive.name, target_dir)
 
 	else:
 		continue
