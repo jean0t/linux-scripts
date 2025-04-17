43fbabf8f87e473d6c86fd942445c8fec6a18315
Jeanot <127698173+jean0t@users.noreply.github.com>
2025-04-17 13:24:23 -0300

    Update organize_downloads.py

--- organization/organize_download_dir/organize_downloads.py
+++ organization/organize_download_dir/organize_downloads.py
@@ -5,57 +5,60 @@ from datetime import datetime
 
 TIMENOW = datetime.now().strftime("%d/%m %H:%M")
 
+
 def message(filename, target_dir):
-	print(f"{TIMENOW} {filename} was moved to {target_dir}")
+    print(f"{TIMENOW} {filename} was moved to {target_dir}")
+
 
 def make_dir(path_obj):
-	path_obj.mkdir(parents=True, exist_ok=True)
+    path_obj.mkdir(parents=True, exist_ok=True)
+
 
 home = Path.home()
 downloads = home.joinpath("Downloads")
 
 for archive in downloads.iterdir():
-	if archive.is_file():
-		extension = archive.suffix
+    if archive.is_file():
+        extension = archive.suffix
         if extension == ".part":
             continue
 
-		match extension:
-			case ".pdf"|".txt"|".md":
-				target_dir = downloads / "texts"
-				make_dir(target_dir)
-				archive.rename(target_dir / archive.name)
-				message(archive.name, target_dir)
+        match extension:
+            case ".pdf" | ".txt" | ".md":
+                target_dir = downloads / "texts"
+                make_dir(target_dir)
+                archive.rename(target_dir / archive.name)
+                message(archive.name, target_dir)
 
-			case ".deb"|".appimage"|"":
-				target_dir = downloads / "binaries"
-				make_dir(target_dir)
-				archive.rename(target_dir / archive.name)
-				message(archive.name, target_dir)
+            case ".deb" | ".appimage" | "":
+                target_dir = downloads / "binaries"
+                make_dir(target_dir)
+                archive.rename(target_dir / archive.name)
+                message(archive.name, target_dir)
 
-			case ".mp4"|".wav"|".mkv":
-				target_dir = downloads / "videos"
-				make_dir(target_dir)
-				archive.rename(target_dir / archive.name)
-				message(archive.name, target_dir)
+            case ".mp4" | ".wav" | ".mkv":
+                target_dir = downloads / "videos"
+                make_dir(target_dir)
+                archive.rename(target_dir / archive.name)
+                message(archive.name, target_dir)
 
-			case ".mp3":
-				target_dir = downloads / "audio"
-				make_dir(target_dir)
-				archive.rename(target_dir / archive.name)
-				message(archive.name, target_dir)
+            case ".mp3":
+                target_dir = downloads / "audio"
+                make_dir(target_dir)
+                archive.rename(target_dir / archive.name)
+                message(archive.name, target_dir)
 
-			case ".jpeg"|".jpg"|".png"|".gif":
-				target_dir = downloads / "images"
-				make_dir(target_dir)
-				archive.rename(target_dir / archive.name)
-				message(archive.name, target_dir)
+            case ".jpeg" | ".jpg" | ".png" | ".gif":
+                target_dir = downloads / "images"
+                make_dir(target_dir)
+                archive.rename(target_dir / archive.name)
+                message(archive.name, target_dir)
 
-			case _:
-				target_dir = downloads / "special"
-				make_dir(target_dir)
-				archive.rename(target_dir / archive.name)
-				message(archive.name, target_dir)
+            case _:
+                target_dir = downloads / "special"
+                make_dir(target_dir)
+                archive.rename(target_dir / archive.name)
+                message(archive.name, target_dir)
 
-	else:
-		continue
+    else:
+        continue
